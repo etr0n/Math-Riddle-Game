a2719868f74a976086be47c0a10b2b6968c00875
etr0n <56270962+etr0n@users.noreply.github.com>
2021-11-03 15:45:00 +0200

    MultiDEX enabled

--- app/src/main/java/com/example/mathriddles/MainActivity.kt
+++ app/src/main/java/com/example/mathriddles/MainActivity.kt
@@ -11,8 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getSupportActionBar()?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        getSupportActionBar()?.setCustomView(R.layout.activity_main);
-
+        getSupportActionBar()?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM)
+        getSupportActionBar()?.setCustomView(R.layout.activity_main)
+        /*MobileAds.initialize(this) {}*/
     }
 }
